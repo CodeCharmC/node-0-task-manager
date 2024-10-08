c94da647bf1e59360b325bcf10cf314997b95532
CodeCharmC <moni.akhtar1066@gmail.com>
2024-10-08 20:56:47 +0600

    Identifiying he routes

--- app.js
+++ app.js
@@ -10,3 +10,10 @@ app.get("/", (req, res) => {
 app.listen(3000, () => {
    console.log("Server is listening on port 3000...");
 });
+
+
+//app.get("/api/v1/tasks") --> get all tasks
+//app.post("/api/v1/tasks") --> create new task
+//app.get("/api/v1/tasks/:id") --> get single task
+//app.patch("/api/v1/tasks/:id") --> update task
+//app.delete("/api/v1/tasks/:id") --> delete task
