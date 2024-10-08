1390a8f0b2f231c0bc63164bcdb021e9e493fe1a
CodeCharmC <moni.akhtar1066@gmail.com>
2024-10-08 21:35:13 +0600

    Create initial controller for all the routes

--- backend/controllers/tasks.js
+++ backend/controllers/tasks.js
@@ -2,4 +2,26 @@ const getAllTask = (req, res) => {
    res.send(`<h1>Task Manager App</h1>`)
 };
 
-module.exports = { getAllTask }
+const createTask = (req, res) => {
+   res.send(`<h1>Create Task</h1>`)
+};
+
+const getTask = (req, res) => {
+   res.send(`<h1>Get Task</h1>`)
+};
+
+const updateTask = (req, res) => {
+   res.send(`<h1>Update Task</h1>`)
+};
+
+const deleteTask = (req, res) => {
+   res.send(`<h1>Delete Task</h1>`)
+};
+
+module.exports = {
+   getAllTask,
+   createTask,
+   getTask,
+   updateTask,
+   deleteTask
+}
