8683e2a472d07e0441e526e719e2967467345058
CodeCharmC <moni.akhtar1066@gmail.com>
2024-10-08 20:39:31 +0600

    frontend: make an express server

--- frontend/app.js
+++ frontend/app.js
@@ -1,1 +1,12 @@
-console.log("Hello, World!");
+const express = require("express");
+const app = express();
+
+//routes
+app.get("/", (req, res) => {
+   res.send(`<h1>Task Manager App</h1>`)
+})
+
+
+app.listen(3000, () => {
+   console.log("Server is listening on port 3000...");
+});
