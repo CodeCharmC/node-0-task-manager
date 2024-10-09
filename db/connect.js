5a929c23b7a2141c654b5405e13618cf10f35b3e
CodeCharmC <moni.akhtar1066@gmail.com>
2024-10-09 20:30:42 +0600

    Connect the mongoose

--- db/connect.js
+++ db/connect.js
@@ -1,1 +1,13 @@
-const connectionString = 'mongodb://localhost:'; 
+const mongoose = require("mongoose");
+
+const connectionString = "mongodb"; 
+
+mongoose
+   .connect(connectionString, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true,
+   })
+   .then(() => { console.log("Connected to DB...") })
+   .catch((err) => { console.log(err) });
