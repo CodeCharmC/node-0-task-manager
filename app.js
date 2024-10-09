0eecc2f55714f8167194c67e9d662a272f52f726
CodeCharmC <moni.akhtar1066@gmail.com>
2024-10-09 20:44:23 +0600

    Start the server with try and catch

--- app.js
+++ app.js
@@ -5,12 +5,20 @@ const task = require("./backend/routes/tasks.js");
 const connectDB = require("./db/connect.js");
 
 //routes
-app.use("/api/v1/tasks", task); //the inisial route
+app.use("/api/v1/tasks", task); 
 
+const start = async () => {
+   try {
+      await connectDB();
+      app.listen(3000, () => {
+         console.log("Server is listening on port 3000...");
+      });
+   } catch (error) {
+      console.log(error);
+   }
+};
 
-app.listen(3000, () => {
-   console.log("Server is listening on port 3000...");
-});
+start();
 
 
 //app.get("/api/v1/tasks") --> get all tasks
