1e9581f02f35bd2e347986ce8b8766836a73d668
CodeCharmC <moni.akhtar1066@gmail.com>
2024-10-17 22:04:12 +0600

    Test createTask using the models

--- backend/controllers/tasks.js
+++ backend/controllers/tasks.js
@@ -5,8 +5,12 @@ const getAllTask = (req, res) => {
 };
 
 const createTask = async (req, res) => {
-   const task = await Task.create(req.body);
-   res.status(201).json({task});
+   try {
+      const task = await Task.create(req.body);
+      res.status(201).json({ task });
+   } catch (error) {
+      res.status(500).json({ error: error.message });
+   }
 };
 
 const getTask = (req, res) => {
