b0fbc7e3323955abbe17a1733d54f92d32e72741
CodeCharmC <moni.akhtar1066@gmail.com>
2024-10-19 11:41:28 +0600

    Update deleteTask with database connection

--- backend/controllers/tasks.js
+++ backend/controllers/tasks.js
@@ -26,7 +26,7 @@ const getTask = async (req, res) => {
       if (!task) {
          return res.status(404).json({ msg: `No task with id : ${taskID}` });
       };
-      
+
       res.status(200).json({ task });  
    } catch (error) {
       res.status(500).json({ error: error.message });
@@ -39,10 +39,21 @@ const updateTask = (req, res) => {
    }) 
 };
 
-const deleteTask = (req, res) => {
-   res.json({
-      id: req.params.id
-   }) 
+const deleteTask = async (req, res) => {
+   try {
+      const { id: taskID } = req.params;
+      const task = await Task.findOneAndDelete({ _id: taskID });
+
+      if (!task) {
+         return res.status(404).json({ msg: `No task with id : ${taskID}` });
+      };
+      
+      res.status(200).json({ task: null, status: 'success' });
+      
+   } catch (error) {
+      res.status(500).json({ error: error.message });      
+   }
+   
 };
 
 module.exports = {
