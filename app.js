63cd8f5d67a582326b10980266e7dc63a17931ae
CodeCharmC <moni.akhtar1066@gmail.com>
2024-12-13 12:16:05 +0600

    some correction

--- app.js
+++ app.js
@@ -32,12 +32,3 @@ const start = async () => {
 };
 
 start();
-
-
-//app.get("/api/v1/tasks") --> get all tasks
-//app.post("/api/v1/tasks") --> create new task
-//app.get("/api/v1/tasks/:id") --> get single task
-//app.patch("/api/v1/tasks/:id") --> update task
-//app.delete("/api/v1/tasks/:id") --> delete task
-
-//REST API = REpresentational State Transfer API
--- backend/routes/tasks.js
+++ backend/routes/tasks.js
@@ -16,3 +16,12 @@ router.route("/").get(getAllTask).post(createTask);
 router.route("/:id").get(getTask).patch(updateTask).delete(deleteTask);
 
 module.exports = router;
+
+
+//app.get("/api/v1/tasks") --> get all tasks
+//app.post("/api/v1/tasks") --> create new task
+//app.get("/api/v1/tasks/:id") --> get single task
+//app.patch("/api/v1/tasks/:id") --> update task
+//app.delete("/api/v1/tasks/:id") --> delete task
+
+//REST API = REpresentational State Transfer API
